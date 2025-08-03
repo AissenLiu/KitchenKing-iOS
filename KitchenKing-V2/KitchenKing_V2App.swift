199ca5bf38428a53b3f9e60373aa65f35b0f0f71
AissenLiu <307407170@qq.com>
2025-08-03 11:06:38 +0800

    Initial Commit

---
+++ KitchenKing-V2/KitchenKing_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenKing_V2App.swift
+//  KitchenKing-V2
+//
+//  Created by 刘琛 on 2025/8/3.
+//
+
+import SwiftUI
+
+@main
+struct KitchenKing_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KitchenKing-V2Tests/KitchenKing_V2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenKing_V2Tests.swift
+//  KitchenKing-V2Tests
+//
+//  Created by 刘琛 on 2025/8/3.
+//
+
+import Testing
+@testable import KitchenKing_V2
+
+struct KitchenKing_V2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
